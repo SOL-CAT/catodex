1445f8ebc58079c5ad88800528f4674cffa30937
natarajanark <natarajanarvind.krishnan@citrix.com>
2021-07-30 03:10:55 -0400

    code to server july 30

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.15c57c5da8d7f4dfa66f7aeff674d654.js"
+  "/precache-manifest.b0973cb277f0cd0104f6e265d99b2102.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
